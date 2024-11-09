f0d4f25d050a23373ce61267cd6509db06575d00
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-11-09 22:29:52 +0100

    Update DEB_UPD_config.sh

--- DEB_UPD_config.sh
+++ DEB_UPD_config.sh
@@ -3,8 +3,9 @@
 ## Sprachwahl
 UV_LNG=DE                                       # Sprache auf Englisch (EN) oder Deutsch (DE) oder Spanisch (SP) umstellen
 #
-## Upgrade-Modes
+## Updater-Modes
 UV_UpgradeMode=false                            # false führt apt-get upgrade -y -a durch / true führt apt-get dist-upgrade -y durch
+UV_AutoremoveMode=false                         # false führt ein --purge durch / true führt ein "remove" durch
 #
 ## LoggingLevel
 UV_LOG=quiet                                    # Loggingstärke angeben (quiet [-qq] / medium [-q] / all [keine output option])
