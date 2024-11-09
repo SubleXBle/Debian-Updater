d4a797d2cb957caf46e28423c1fe9b479fdb1a84
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-11-09 20:33:00 +0100

    Update varchecktwo.sh

--- Files/varchecktwo.sh
+++ Files/varchecktwo.sh
@@ -20,25 +20,33 @@ source "$config_file"
 # Check for Variable Data
 # Language
 if [[ "$UV_LNG" != "DE" && "$UV_LNG" != "EN" && "$UV_LNG" != "SP" ]]; then
-    echo "ERROR: UV_LNG has to be 'DE', 'EN' or 'SP' but it is: $UV_LNG"
+    log_message "ERROR: UV_LNG has to be 'DE', 'EN' or 'SP' but it is: $UV_LNG"
+    echo "ERROR: UV_LNG has to be 'DE', 'EN' or 'SP' but it is: $UV_LNG" >> $LOGFILE
+    exit 1
 else
     echo "Die Variable UV_LNG ist korrekt gesetzt auf $UV_LNG." >> /dev/null
 fi
 # LogLevel Check
 if [[ "$UV_LOG" != "quiet" && "$UV_LOG" != "medium" && "$UV_LOG" != "all" ]]; then
-    echo "ERROR: UV_LOG has to be 'quiet', 'medium' or 'all' but it is set to: $UV_LOG"
+    log_message "ERROR: UV_LOG has to be 'quiet', 'medium' or 'all' but it is set to: $UV_LOG"
+    echo "ERROR: UV_LOG has to be 'quiet', 'medium' or 'all' but it is set to: $UV_LOG" >> $LOGFILE
+    exit 1
 else
     echo "Die Variable UV_LOG ist korrekt gesetzt auf $UV_LOG." >> /dev/null
 fi
 # Path Validation
 if ! is_valid_path "$LOGFILE"; then
-    echo "ERROR: LOGFILE is not a valid Path: $LOGFILE"
+    log_message "ERROR: LOGFILE is not a valid Path: $LOGFILE"
+    echo "ERROR: LOGFILE is not a valid Path: $LOGFILE" >> $LOGFILE
+    exit 1
 else
     echo "Die Variable LOGFILE ist korrekt gesetzt auf einen gültigen Pfad: $LOGFILE." >> /dev/null
 fi
 # Check if Logfile max age is a number
 if ! [[ "$LOGFILE_MAX_AGE" =~ ^[0-9]+$ ]]; then
-    echo "ERROR: LOGFILE_MAX_AGE has to be a valid number but it is set to: $LOGFILE_MAX_AGE"
+    log_message "ERROR: LOGFILE_MAX_AGE has to be a valid number but it is set to: $LOGFILE_MAX_AGE"
+    echo "ERROR: LOGFILE_MAX_AGE has to be a valid number but it is set to: $LOGFILE_MAX_AGE" >> $LOGFILE
+    exit 1
 else
     echo "Die Variable LOGFILE_MAX_AGE ist korrekt gesetzt auf $LOGFILE_MAX_AGE." >> /dev/null
 fi
