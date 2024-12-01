3d896767a584a96eebc54f9932ab3b10d3ee8e9c
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-01 20:31:49 +0100

    Update Nextcloud.sh

--- Files/Nextcloud.sh
+++ Files/Nextcloud.sh
@@ -1,6 +1,6 @@
 
 NC_APP_UPDATE() {
-  if exists $UV_NC_OCC_PATH; then
+  if [ -d "$UV_NC_OCC_PATH" ]; then
     log_message -n "$LV_NC_APPS: \t \t \t \t "
     sudo -u www-data php $UV_NC_OCC_PATH/occ app:update --all >> $LOGFILE
     LF_Positive_Output_Check
