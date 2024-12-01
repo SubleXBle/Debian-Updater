c03860a512836d0a89ce4fd7f8d656eef12dfcf5
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-01 17:30:11 +0100

    Update Nextcloud.sh

--- Files/Nextcloud.sh
+++ Files/Nextcloud.sh
@@ -1,11 +1,11 @@
 
 NC_APP_UPDATE() {
-if exists $UV_NC_OCC_PATH; then
-  log_message -n "$LV_NC_APPS: \t \t \t \t "
-  sudo -u www-data php $UV_NC_OCC_PATH/occ app:update
-  LF_Positive_Output_Check
-else
-  FEHLER=Nextcloud-NotFound
-  LF_Negative_Output_Check
-fi
+  if exists $UV_NC_OCC_PATH; then
+    log_message -n "$LV_NC_APPS: \t \t \t \t "
+    sudo -u www-data php $UV_NC_OCC_PATH/occ app:update
+    LF_Positive_Output_Check
+  else
+    FEHLER=Nextcloud-NotFound
+    LF_Negative_Output_Check
+  fi
 }
