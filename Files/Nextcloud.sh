1efb5a61c48defaad049fc36265ff849e5fdd494
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-01 17:26:35 +0100

    Update Nextcloud.sh

--- Files/Nextcloud.sh
+++ Files/Nextcloud.sh
@@ -1,7 +1,6 @@
+
 if exists $UV_NC_OCC_PATH; then
+  log_message -n "$LV_NC_APPS: \t \t \t \t "
   sudo -u www-data php $UV_NC_OCC_PATH/occ app:update
+  LF_Positive_Output_Check
 fi
-
-
-UV_NC_APP_Update=false                          # Wenn auf true gesetzt, wird das Script ebenfalls App-Updates für Nextcloud ausführen
-UV_NC_OCC_PATH="/path/to/your/Nextcloud"
