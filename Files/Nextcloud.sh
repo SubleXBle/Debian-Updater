9aca7a5feaaddb2cfb4e444c9bc9e5e5b5964aab
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-01 20:26:29 +0100

    Update Nextcloud.sh

--- Files/Nextcloud.sh
+++ Files/Nextcloud.sh
@@ -2,7 +2,7 @@
 NC_APP_UPDATE() {
   if exists $UV_NC_OCC_PATH; then
     log_message -n "$LV_NC_APPS: \t \t \t \t "
-    sudo -u www-data php $UV_NC_OCC_PATH/occ app:update >> $LOGFILE
+    sudo -u www-data php $UV_NC_OCC_PATH/occ app:update --all >> $LOGFILE
     LF_Positive_Output_Check
   else
     FEHLER=Nextcloud-NotFound
