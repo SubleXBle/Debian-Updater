ae811f40ec0c43e73b8486e1c687c3967dbd77fd
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-14 10:26:27 +0100

    Update GOTIFY_config.sh

--- NotificationConfiguration/GOTIFY_config.sh
+++ NotificationConfiguration/GOTIFY_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GOTIFY_URL="https://dein-gotify-server.com/message"  # URL deines Gotify-Servers
-GOTIFY_TOKEN="dein_gotify_token"                     # Dein Gotify-Token
-GO_PRIO=5                                    # Standardpriorität (optional)
-GO_TITLE="Updates"                                   # Titel der Benachrichtigung
+GOTIFY_URL="https://dein-gotify-server.com/message"  # URL : Gotify-Server
+GOTIFY_TOKEN="dein_gotify_token"                     # Gotify-Token
+GO_PRIO=5                                            # Priority (optional)
+GO_TITLE="Updates"                                   # Subject / Title
 BENACHRICHTIGUNG="Update ausgeführt"                 # Your Text-Message if not changed to send logfile
