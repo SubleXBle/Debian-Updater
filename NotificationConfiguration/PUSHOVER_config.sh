e84ed2fb2e2848945e431ee1fcdbd85ba8b1cd81
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-14 10:25:33 +0100

    Update PUSHOVER_config.sh

--- NotificationConfiguration/PUSHOVER_config.sh
+++ NotificationConfiguration/PUSHOVER_config.sh
@@ -4,7 +4,7 @@
 PO_MAX_RETRY=3                                      # How often should I retry to send a message if something goes wrong?
 PO_TOKEN='DEIN-PUSHOVER-APP-TOKEN'                  # welche App sendet (Application Token)
 PO_USER='DEIN-PUSHOVER-USER-TOKEN'                  # Empfänger (User Key)
-PO_TITLE='Updates auf Webserver'                    # Betreff der Nachricht
+PO_TITLE='Updates auf Webserver'                    # Subject
 BENACHRICHTIGUNG='Updates auf Webserver ausgeführt' # Your Text-Message if not changed to send logfile
 PO_LINKTITLE=''                                     # Linktitel / Linkname
 PO_LINK=''                                          # Link (URL)
