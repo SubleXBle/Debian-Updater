631dfdf7981b9d4fa9a9f62f7db03029eba945e2
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-14 10:24:57 +0100

    Update TELEGRAM_config.sh

--- NotificationConfiguration/TELEGRAM_config.sh
+++ NotificationConfiguration/TELEGRAM_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BOT_TOKEN="TELEGRAM-BOT-TOKEN"            # Dein Telegram Bot Token
-CHAT_ID="TELEGRAM-CHAT-ID"                # Deine Telegram Chat ID
+BOT_TOKEN="TELEGRAM-BOT-TOKEN"            # Telegram Bot Token
+CHAT_ID="TELEGRAM-CHAT-ID"                # Telegram Chat ID
 TG_MAX_RETRY="3"                          # How often should I retry to send a message if something goes wrong?
 BENACHRICHTIGUNG="Updates ausgeführt"     # Your Text-Message if not changed to send logfile
