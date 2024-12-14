d9d664fee16cbe332d2ca43496c6e9e6692cc4bc
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2024-12-14 11:24:44 +0100

    Update varcheckone.sh

--- Files/varcheckone.sh
+++ Files/varcheckone.sh
@@ -11,7 +11,7 @@ check_them_files() {
     #echo "running check ..."
     for file in "${FILES[@]}"; do
         if [ -f "$file" ]; then
-            echo "✅ Die Datei '$file' ist vorhanden. >> $LOGFILE
+            echo "✅ Die Datei '$file' ist vorhanden. >> /dev/null
         else
             echo "❌ Could NOT find '$file' !"
         fi
